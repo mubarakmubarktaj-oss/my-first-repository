e97f6ef1752e68b6a0a40584773e1a26b1f1fdb1
mubarakmubarktaj-oss <mubarakmubarktaj@gmail.com>
2026-06-26 17:05:17 +0530

    Add files via upload

---
+++ 6 subjects program.py
@@ -0,0 +1,12 @@
+m1=int(input("enter computer science marks"))
+m2=int(input("enter economic marks"))
+m3=int(input("enter business marks"))
+m4=int(input("enter acounts marks"))
+m5=int(input("enter english marks"))
+m6=int(input("enter hindi marks"))
+total=m1+m2+m3+m4+m5+m6
+if total> 200:
+    print("pass")
+else:
+    print("fail")
+    
